54585c5965b462de7579704e1374e8a8be849bcc
devmaffi <devmaffi550@gmail.com>
2022-04-08 10:32:27 +0300

    Implement scroll-up handler

--- src/assets/js/modules/scrollHandler.js
+++ src/assets/js/modules/scrollHandler.js
@@ -9,10 +9,23 @@ function scrollHeader() {
   else header.classList.remove('scroll-header')
 }
 
+// Handle scroll-up module representation
+
+// this reefers to window
+function showScrollUp() {
+  const scrollUp = document.querySelector('#scroll-up')
+
+  // when the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if (this.scrollY >= 350) return scrollUp.classList.add('show-scroll')
+
+  return scrollUp.classList.remove('show-scroll')
+}
+
 // Add event handlers on window
 
 function handleScroll() {
   window.addEventListener('scroll', scrollHeader)
+  window.addEventListener('scroll', showScrollUp)
 }
 
 // Exports
